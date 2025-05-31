2f8eec090eacdf3f2aaf716f20a510aa9fe059f5
mohammed mujeeb ahmed <mohammed.mujeebmokhtar@gmail.com>
2025-05-31 19:18:10 +0530

    web

---
+++ 1.js
@@ -0,0 +1,27 @@
+console.log('test from the web page');
+var x;
+console.log(x);
+console.log(typeof x);
+
+x= 'some string';
+console.log(x);
+console.log(typeof x);
+
+x = new Date();
+console.log(x);
+console.log(typeof x);
+
+x = null;
+console.log(x);
+console.log(typeof x);
+
+let y= 1;
+console.log('y is:',y);
+console.log('the type of y is:',typeof y); 
+
+const z = 'zed';
+console.log('z is:',z);
+console.log('the type of z is:',typeof z);
+ 
+
+
